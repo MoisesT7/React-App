1df37932716e64e2d8c03fba574c142ef0c4c821
Moises Taboada <moises.th.1107@gmail.com>
2022-06-18 15:06:33 -0500

    Rendering components in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ListOfGifs from "./components/ListOfGifs";
+import { Link, Route } from "wouter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <h1>App</h1>
+        <Link to="/gifs/peru">Gifs de Peru</Link>
+        <Link to="/gifs/colombia">Gifs de Colombia</Link>
+        <Link to="/gifs/brasil">Gifs de Brasil</Link>
+        <Route component={ListOfGifs} path="/gifs/:keyword" />
+      </section>
     </div>
   );
 }
